2443b5bcaa9debcf5187478725f7551852bebcfd
Hyeonji-Jung <welloff.jj@gmail.com>
2021-02-20 23:04:19 +0900

    ex07git add .

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/20 18:44:32 by hyeojung          #+#    #+#             */
+/*   Updated: 2021/02/20 23:03:59 by hyeojung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		strlength(int nb)
+{
+	int length;
+
+	length = 1;
+	if (nb < 0)
+		length++;
+	while (nb / 10 != 0)
+	{
+		nb /= 10;
+		length++;
+	}
+	return (length);
+}
+
+void	ft_putnbr(int nb)
+{
+	int len;
+	int number;
+	char num[13];
+
+	len = strlength(nb);
+	number = nb;
+	while (len > 0)
+	{
+		if (nb >= 0)
+			num[len - 1] = (nb % 10) + '0';
+		else
+			num[len - 1] = (nb % 10) * -1 + '0';
+		if (nb / 10 != 0)
+			nb /= 10;
+		len--;
+	}
+	if (nb >= 0)
+		num[0] = nb + '0';
+	else
+		num[0] = '-';
+	write(1, num, strlength(number));
+}
+
+int		main(void)
+{
+	ft_putnbr(2147483647);
+	return (0);
+}
