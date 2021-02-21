a45d7e3e06040d365223663642820cf973aa10c5
hyeojung <welloff.jj@gmail.com>
2021-02-21 22:26:22 +0900

    huu

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -1,24 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/21 18:16:22 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/02/21 21:58:10 by hyeojung         ###   ########.fr       */
+/*   Created: 2021/02/21 21:58:46 by hyeojung          #+#    #+#             */
+/*   Updated: 2021/02/21 21:58:56 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	index;
 
-	index = 0;
-	while(src[index++] != '\0')
-	{
-		dest[index] = src[index];
-	}
-	dest[index] = '\0';
-	return (dest);
-}
