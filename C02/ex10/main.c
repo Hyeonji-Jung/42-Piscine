1035644c11660a48777bb37c0063c629790ffe59
hyeojung <welloff.jj@gmail.com>
2021-02-25 16:13:37 +0900

    main

--- C02/ex10/main.c
+++ C02/ex10/main.c
@@ -6,7 +6,7 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 13:16:32 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/02/25 13:20:10 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/02/25 15:41:47 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int				main(void)
 	char dest[100] = {0,};
 	char *src = "0123456789";
 
-	printf("%d\n", ft_strlcpy(dest, src, 5));
+	printf("%d\n", ft_strlcpy(dest, src, -1));
 	printf("%s\n", dest);
 	return 0;
 }
