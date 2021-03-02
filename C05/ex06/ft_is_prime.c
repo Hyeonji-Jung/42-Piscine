8e6aec32f10e4e3599c1a6655dce7a1b0e6f9ae2
Hyeonji-Jung <welloff.jj@gmail.com>
2021-03-02 17:59:07 +0900

    ~ex05

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -6,18 +6,32 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 17:14:37 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/03/02 17:20:39 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/03/02 17:58:42 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int 	ftt_sqrt(int nb)
+{
+	int	i;
+
+	i = 1;
+	while (i * i < nb)
+		i++;
+	return (i);
+}
 
 int		ft_is_prime(int nb)
 {
 	int i;
 
-	i = 0;
+	i = 4;
 	if (nb <= 0 || nb == 1)
 		return (0);
-	while (i < ft_sqrt(nb))
+	while (i < ftt_sqrt(nb) + 1)
 	{
+		if (nb % i == 0)
+			return (0);
+		i++;
 	}
+	return (1);
 }
