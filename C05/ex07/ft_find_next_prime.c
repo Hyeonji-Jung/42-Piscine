26456ea714b453efe4bdad02a5faf8932f45b117
Hyeonji-Jung <welloff.jj@gmail.com>
2021-03-04 13:11:59 +0900

    modified

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,30 +6,39 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 17:59:43 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/03/02 18:23:30 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/03/04 13:11:46 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_find_next_prime(int nb)
+int		ftt_sqrt(int nb)
+{
+	long long	i;
+
+	i = 1;
+	while (i * i < nb)
+		i++;
+	return (i);
+}
+
+int		ft_is_prime(int nb)
 {
 	int	i;
-	int	cnt;
 
-	if (nb < 3)
-		return (2);
-	while (nb)
+	i = 4;
+	if (nb == 0 || nb == 1)
+		return (0);
+	while (i < ftt_sqrt(nb) + 1)
 	{
-		i = 3;
-		cnt = 0;
-		while (i < nb)
-		{
-			if (nb % i == 0)
-				cnt++;
-			i++;
-		}
-		if (cnt == 0)
-			break;
-		nb++;
+		if (nb % i == 0)
+			return (0);
+		i++;
 	}
+	return (1);
+}
+
+int		ft_find_next_prime(int nb)
+{
+	while (!(ft_is_prime(nb)))
+		nb++;
 	return (nb);
 }
