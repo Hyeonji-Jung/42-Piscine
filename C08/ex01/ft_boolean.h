b1a8308790add5cd99c82dcde5252bceae45a160
Hyeonji-Jung <welloff.jj@gmail.com>
2021-03-08 15:19:59 +0900

    chk norm

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,21 +6,21 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 18:11:46 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/03/03 18:29:16 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/03/08 15:07:13 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
-#define FT_BOOLEAN_H
+# define FT_BOOLEAN_H
 
-#include <unistd.h>
+# include <unistd.h>
 
 typedef int	t_bool;
-#define EVEN(nb) (nb % 2 == 0) ? 0 : 1
-#define TRUE 1
-#define FALSE 0
-#define EVEN_MSG "I have an even number of arguments.\n"
-#define ODD_MSG "I have an odd number of arguments.\n"
-#define SUCCESS 0
+# define EVEN(nb) (nb % 2 == 0) ? 0 : 1
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
 
 #endif
