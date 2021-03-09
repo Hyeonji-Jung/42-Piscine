8fed5eaa60f759025c386798e4581b0e6ebba98c
hyeojung <welloff.jj@gmail.com>
2021-03-09 21:11:25 +0900

    ??

--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -6,11 +6,17 @@
 /*   By: hyeojung <hyeojung@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 21:01:18 by hyeojung          #+#    #+#             */
-/*   Updated: 2021/03/09 21:05:05 by hyeojung         ###   ########.fr       */
+/*   Updated: 2021/03/09 21:11:17 by hyeojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+int		g_queens[10];
+
+void	rec(void)
+{
+}
 
 int		ft_ten_queens_puzzle(void)
 {
